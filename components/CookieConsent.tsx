3682818b9f6c48b2744f49963e60e9658a88d980
maciopelo <maciej.k00h@gmail.com>
2025-08-02 00:28:50 +0200

    style cookies

--- components/CookieConsent.tsx
+++ components/CookieConsent.tsx
@@ -20,17 +20,17 @@ export default function CookieConsent() {
   if (!visible) return null;
 
   return (
-    <div className="fixed right-4 bottom-4 left-4 z-50 max-w-md rounded-lg bg-gray-800 p-4 text-white shadow-lg md:right-auto md:bottom-8 md:left-8">
+    <div className="fixed right-2 bottom-2 left-2 z-50 max-w-md rounded-lg bg-stone-950/85 p-4 text-stone-200 shadow-lg md:right-auto md:bottom-8 md:left-8">
       <p className="text-sm">
-        This website uses cookies to enhance the user experience. By using the
-        site, you agree to our use of cookies.
+        Ta strona uzywa plików cookie, aby poprawić komfort użytkowania.
+        Korzystając z tej strony, zgadzasz się na używanie plików cookie.
       </p>
       <div className="mt-2 flex justify-end">
         <button
           onClick={acceptCookies}
-          className="rounded bg-blue-500 px-4 py-1 text-sm text-white hover:bg-blue-600"
+          className="cursor-pointer rounded bg-stone-200 px-4 py-1 text-sm text-stone-700 hover:bg-stone-700 hover:text-stone-200"
         >
-          Accept
+          Akceptuj
         </button>
       </div>
     </div>
